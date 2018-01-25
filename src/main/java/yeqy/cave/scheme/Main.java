4cda348d5ebc00fcdd5d67d81a41e951ab2e9df9
yeqingyun <yeqy@gionee.com>
2018-01-25 18:30:20 +0800

    init
    Signed-off-by: yeqingyun <yeqy@gionee.com>

--- src/main/java/yeqy/cave/scheme/Main.java
+++ src/main/java/yeqy/cave/scheme/Main.java
@@ -1,10 +1,22 @@
 package yeqy.cave.scheme;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
- * Created by yeqy on 2018/1/25.
+ * Created by yeqy on 2017/12/25.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Cave Scheme Version 0.1");
+        System.out.println("made by yeqy, Let's make a Joy.\n");
+        System.out.print(">> ");
+        try (BufferedReader codeReader = new BufferedReader(new InputStreamReader(System.in))){
+            while (true) {
+                System.out.println(codeReader.readLine());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
