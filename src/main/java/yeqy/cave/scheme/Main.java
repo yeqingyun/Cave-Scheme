5489f84781aa810b8b4288a6b2722b648534e255
yeqingyun <yeqy@gionee.com>
2018-01-25 18:07:51 +0800

    init

---
+++ src/main/java/yeqy/cave/scheme/Main.java
@@ -0,0 +1,10 @@
+package yeqy.cave.scheme;
+
+/**
+ * Created by yeqy on 2018/1/25.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Cave Scheme Version 0.1");
+    }
+}
