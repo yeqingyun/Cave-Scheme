bfaeca011a17fdbc8b1089534eedc3cd64bbc156
yeqingyun <yeqy@gionee.com>
2018-01-29 16:33:20 +0800

    Signed-off-by: yeqingyun <yeqy@gionee.com>

--- src/main/java/yeqy/cave/scheme/Main.java
+++ src/main/java/yeqy/cave/scheme/Main.java
@@ -1,23 +1,18 @@
 package yeqy.cave.scheme;
 
-import yeqy.cave.scheme.exception.MethodIsNotDefinedException;
-import yeqy.cave.scheme.exception.ParameterException;
-import yeqy.cave.scheme.exception.SyntaxException;
 import yeqy.cave.scheme.paser.TokenPaser;
 import yeqy.cave.scheme.structure.Environment;
 import yeqy.cave.scheme.structure.SExpression;
 import yeqy.cave.scheme.type.BaseType;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by yeqy on 2017/12/25.
  */
 public class Main {
-    public static void main(String[] args) throws InvocationTargetException, ParameterException, MethodIsNotDefinedException, SyntaxException, IllegalAccessException {
+    public static void main(String[] args) {
         System.out.println("Cave Scheme Version 0.1");
         System.out.println("made by yeqy, Let's make a joy.\n");
         try (BufferedReader codeReader = new BufferedReader(new InputStreamReader(System.in))) {
@@ -45,11 +40,10 @@ public class Main {
                     if (baseType != null)
                         System.out.println(baseType);
                 } catch (Exception e) {
-                    e.printStackTrace();
                     System.out.println(e.getMessage());
                 }
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
